195806f1d32bb8e3156744119e3719986090d2eb
Yurii Topin <ytopin@griddynamics.com>
2025-09-25 22:41:05 +0000

    Add Google license header

--- cc/tests/gpu/sycl/vector_sycl.h
+++ cc/tests/gpu/sycl/vector_sycl.h
@@ -1,6 +1,17 @@
-//
-// Created by yuriit on 9/13/25.
-//
+/* Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+============================================================================== */
 
 #ifndef VECTOR_SYCL_H
 #define VECTOR_SYCL_H
@@ -8,8 +19,6 @@
 #include <sycl/sycl.hpp>
 #include <iostream>
 #include <vector>
-
-//namespace sycl = cl::sycl;
 
 int VectorGenerateAndSum(int size);
 
